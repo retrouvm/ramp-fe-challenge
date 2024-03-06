dd50838ef793c06f3d95c00756ac7b5d711e7fbd
retrouvm <retrouvaillesm@gmail.com>
2024-03-06 12:54:08 -0600

    resetting original bug#1

--- src/components/InputSelect/index.tsx
+++ src/components/InputSelect/index.tsx
@@ -71,6 +71,7 @@ export function InputSelect<TItem>({
                 "RampInputSelect--dropdown-container-opened": isOpen,
               })}
               {...getMenuProps()}
+              style={{ top: dropdownPosition.top, left: dropdownPosition.left }}
             >
               {renderItems()}
             </div>
